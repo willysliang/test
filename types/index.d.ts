b7e11824b09221a0c3c6aa50b5249daf9e049fb3
willy <willysliang@qq.com>
2024-09-25 16:21:40 +0800

    test: blog build test

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-// 声明下述文件为模块，不然在typescript-eslint无法识别
-declare module '*.png'
-declare module '*.gif'
-declare module '*.jpg'
-declare module '*.webp'
-declare module '*.apng'
-declare module '*.svga'
-declare module '*.avif'
-declare module '*.scss'
--- types/vue-shims.d.ts
+++
@@ -1,5 +0,0 @@
-declare module '*.vue' {
-  import { type Component } from 'vue'
-  const component: Component | JSX.Element | JSX.IntrinsicElements | any
-  export default component
-}
